3d6f5a96211fb3a39eff0fe66fff82b4ef72a7dd
shlomo ruckban <shlomo.r@onoapps.com>
2023-04-03 17:06:58 +0300

    corrected exprience

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
         <div className="AboutMeTitle">Hey, I'm Shlomo, Nice to meet you. </div>
         <div className="AboutMeDescription">
           <span>
-            I've started my journey 8 months ago, I've done projects solo and
+            I've started working at the 1st of August 2022, I've done projects solo and
             collaborated with people. Developed Apps and Websites. I'm quietly
             confident, naturally curious, and perpetually working on improving
             my chops one design problem at a time.
